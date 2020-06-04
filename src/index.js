0ffefc9ea1ee6a267445c27e0f03febc42bfedab
zhihli <zhli@voxelcloud.net.cn>
2020-06-04 18:13:38 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react'
+export const AutoTextArea = ({className, defaultValue, placeholder, onChange}) => {
+    const outerRef = useRef();
+    const textRef = useRef();
+    useEffect(() => {
+        const autoHeight = () => {
+            const target = textRef.current;
+            target.style.height = 'auto';
+            target.style.height = target.scrollHeight + 'px';
+            outerRef.current.style.height = target.scrollHeight + 'px';
+        }
+        if (textRef.current) {
+            autoHeight();
+            textRef.current.addEventListener('input', autoHeight);
+        }
+        return () => {
+            if (textRef.current) {
+                textRef.current.removeEventListener('input', autoHeight)
+            }
+        }
+    }, [])
+    return <div className={className} ref={outerRef}><textarea ref={textRef} placeholder={placeholder} defaultValue={defaultValue} onChange={onChange}></textarea></div>
+}
+AutoTextArea.defaultProps = {
+    className: '',
+    defaultValue: '',
+    placeholder: 'enter...',
+    onChange: () => {}
+}
